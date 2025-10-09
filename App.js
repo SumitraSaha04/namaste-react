f91848ae953a94025dca5c02c1c0376953bf33ef
Sumitra Saha <sahaankita711204@gmail.com>
2025-10-09 18:48:51 +0530

    3rd Commit

--- App.js
+++ App.js
@@ -1,32 +1,42 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement(
-  "div",
-  {
-    id: "parent",
-  },
-  [
-    React.createElement(
-    "div",
-    { id: "child1" },
-    [
-      React.createElement("h2",{},"I am also an element"),
-        React.createElement("h1",{},"I am h1 tag"),
-        React.createElement("h2",{},"I am h2 tag")
-    ]
-  ),
-React.createElement(
-    "div",
-    { id: "child2" },
-    [
-        React.createElement("h1",{},"I am h1 tag"),
-        React.createElement("h2",{},"I am h2 tag")
-    ]
-  )
-  ]
-  
+//React Element
+const heading = <h1 id="jsx">Namaste React using JSX</h1>;
+console.log(heading); // object-To make react element to component just use arrow fuunc and heading should be Heading
+
+//React Component--JS functions - components returns jsx code or react element for single line jsx code
+const HeadingComponent1=()=><h1>Namaste React Using Functional Component </h1>;
+
+//React Component for multiple lines
+// const HeadingComponent2 = () => {
+//   return (
+//     <div>
+//       <h1>Namaste React Using React Component2</h1>
+//       <HeadingComponent1/>
+//       <h2>Namaste React Using React COmponent</h2>
+//     </div>
+//   );
+// };
+
+const number=1000;
+const Title=()=>(
+    <div className="child">
+        <h1>
+            Namaste React of Title Component 
+        </h1>
+    </div>
 );
-console.log(parent);
+
+
+const HeadingComponent3=()=>(
+    <div className="parent">
+        {Title()}
+        {number * number}
+        <Title/>
+        <Title></Title>
+        <h1>Namaste React of HeadingComponent3</h1>
+
+    </div>
+)
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<HeadingComponent3/>);
