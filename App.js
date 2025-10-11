39fc4efc80c2d8e71af01b19f13413708176abe8
Sumitra Saha <sahaankita711204@gmail.com>
2025-10-12 01:53:31 +0530

    Made Header

--- App.js
+++ App.js
@@ -1,42 +1,44 @@
 import ReactDOM from "react-dom/client";
+const Header=()=>{
+    return (
+        <div className="header-container">
+            <div className="logo-container">
+                <img alt="Restro Food Logo" className="logo" src="https://graphicsfamily.com/wp-content/uploads/edd/2021/06/Editable-Photoshop-Food-Logo-Design-PNG-Transparent.png"/>
+            </div>
+            <div className="nav-links">
+                <ul>
+                    <li>
+                        Home
+                    </li>
+                     <li>
+                        Famous Restro Food
+                    </li>
+                     <li>
+                        About 
+                    </li>
+                     <li>
+                        Contact Us
+                    </li>
+                     <li>
+                        Cart
+                    </li>
+                </ul>
 
-//React Element
-const heading = <h1 id="jsx">Namaste React using JSX</h1>;
-console.log(heading); // object-To make react element to component just use arrow fuunc and heading should be Heading
-
-//React Component--JS functions - components returns jsx code or react element for single line jsx code
-const HeadingComponent1=()=><h1>Namaste React Using Functional Component </h1>;
-
-//React Component for multiple lines
-// const HeadingComponent2 = () => {
-//   return (
-//     <div>
-//       <h1>Namaste React Using React Component2</h1>
-//       <HeadingComponent1/>
-//       <h2>Namaste React Using React COmponent</h2>
-//     </div>
-//   );
-// };
-
-const number=1000;
-const Title=()=>(
-    <div className="child">
-        <h1>
-            Namaste React of Title Component 
-        </h1>
-    </div>
-);
+            </div>
 
 
-const HeadingComponent3=()=>(
-    <div className="parent">
-        {Title()}
-        {number * number}
-        <Title/>
-        <Title></Title>
-        <h1>Namaste React of HeadingComponent3</h1>
+        </div>
 
-    </div>
-)
+    );
+}
+
+
+
+const  AppLayout=()=>{
+    return (
+Header()
+
+    );
+}
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HeadingComponent3/>);
+root.render(<AppLayout/>);
