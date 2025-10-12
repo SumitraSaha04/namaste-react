fa7d796e298428fcb4b0df5fac9ccfc087057b2f
Sumitra Saha <sahaankita711204@gmail.com>
2025-10-13 02:13:59 +0530

    body created

--- App.js
+++ App.js
@@ -1,44 +1,68 @@
 import ReactDOM from "react-dom/client";
-const Header=()=>{
-    return (
-        <div className="header-container">
-            <div className="logo-container">
-                <img alt="Restro Food Logo" className="logo" src="https://graphicsfamily.com/wp-content/uploads/edd/2021/06/Editable-Photoshop-Food-Logo-Design-PNG-Transparent.png"/>
-            </div>
-            <div className="nav-links">
-                <ul>
-                    <li>
-                        Home
-                    </li>
-                     <li>
-                        Famous Restro Food
-                    </li>
-                     <li>
-                        About 
-                    </li>
-                     <li>
-                        Contact Us
-                    </li>
-                     <li>
-                        Cart
-                    </li>
-                </ul>
+const Header = () => {
+  return (
+    <div className="header-container">
+      <div className="logo-container">
+        <img
+          alt="Restro Food Logo"
+          className="logo"
+          src="https://graphicsfamily.com/wp-content/uploads/edd/2021/06/Editable-Photoshop-Food-Logo-Design-PNG-Transparent.png"
+        />
+      </div>
+      <div className="nav-links">
+        <ul>
+          <li>Home</li>
+          <li>Famous Restro Food</li>
+          <li>About</li>
+          <li>Contact Us</li>
+          <li>Cart🛒</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+const styleCard = {
+  backgroundColor: "white",
+};
 
-            </div>
+const RestroCard = () => {
+  return (
+    <div className="restro-card" style={styleCard}>
+      <img
+        className="food-img"
+        alt="food-img"
+        src="https://i.pinimg.com/736x/ce/ca/44/ceca445df991a3eeaa49861b7cd2db7c.jpg"
+      />
+      <h3>Meghna Foods</h3>
+      <h4>Italian Cuisine</h4>
+      <h4>
+        4.4 stars 
+      </h4>
+      <h4>30 mins</h4>
 
+      <div className="rating"></div>
+    </div>
+  );
+};
 
-        </div>
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        <RestroCard />
+      </div>
+    </div>
+  );
+};
 
-    );
-}
-
-
-
-const  AppLayout=()=>{
-    return (
-Header()
-
-    );
-}
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppLayout/>);
+root.render(<AppLayout />);
