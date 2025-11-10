b14c63fcee9db186f9b2b826ed1d3fe00b8591b2
Sumitra Saha <sahaankita711204@gmail.com>
2025-11-10 22:11:01 +0530

    Did Routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
+import { createBrowserRouter } from "react-router-dom";
+import { RouterProvider } from "react-router";
+import About from "./components/About";
+import Error from "./components/Error";
+import Contact from "./components/Contact";
+
 
 const AppLayout = () => {
   return (
@@ -11,5 +17,22 @@ const AppLayout = () => {
   );
 };
 
+const appRouter=createBrowserRouter([{
+  path:"/",
+  element:<AppLayout/>,
+  errorElement:<Error/>
+
+},{
+  path:"/about",
+  element:<About/>,
+  errorElement:<Error/>
+
+},{
+  path:"/contact",
+  element:<Contact/>,
+  errorElement:<Error/>
+
+}]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppLayout />);
+root.render(<RouterProvider router={appRouter}/>);
---
+++ src/components/Error.jsx
@@ -0,0 +1,17 @@
+import { useRouteError } from "react-router-dom";
+
+import React from 'react'
+
+const Error = () => {
+    const err=useRouteError();
+    console.log(err);
+  return (
+    <div>
+      <h1>  {err.status}</h1>
+      <h2>{err.statusText}</h2>
+      
+    </div>
+  )
+}
+
+export default Error;
